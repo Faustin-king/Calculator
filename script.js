0206499ebc9337bc4f64b3d4f8896b2b03e71077
Faustin_King <f.nduwumwamiwabami@gmail.com>
2023-05-12 08:44:01 +0200

    Add subtract, add, multiply & divide functions

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(a, b) {
+  return a + b;
+}
+
+function sutract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
